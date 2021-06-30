3ace894f540cd56ded0d1286e9796c38a06a6dff
TuanAnh <19521219@gm.uit.edu.vn>
2021-06-30 20:10:01 +0700

    Add project files.

---
+++ Server/Server.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Server));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lvSearchHistory = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbReceived = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWork = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(72, 390);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(193, 93);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lvSearchHistory
+            // 
+            this.lvSearchHistory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvSearchHistory.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvSearchHistory.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.lvSearchHistory.HideSelection = false;
+            this.lvSearchHistory.Location = new System.Drawing.Point(336, 151);
+            this.lvSearchHistory.Margin = new System.Windows.Forms.Padding(6, 9, 6, 9);
+            this.lvSearchHistory.Name = "lvSearchHistory";
+            this.lvSearchHistory.Size = new System.Drawing.Size(289, 391);
+            this.lvSearchHistory.TabIndex = 14;
+            this.lvSearchHistory.UseCompatibleStateImageBehavior = false;
+            this.lvSearchHistory.View = System.Windows.Forms.View.List;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(830, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 44);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Result";
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(655, 151);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(483, 391);
+            this.rtbResult.TabIndex = 12;
+            this.rtbResult.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 44);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Received";
+            // 
+            // tbReceived
+            // 
+            this.tbReceived.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbReceived.Location = new System.Drawing.Point(37, 151);
+            this.tbReceived.Multiline = true;
+            this.tbReceived.Name = "tbReceived";
+            this.tbReceived.Size = new System.Drawing.Size(272, 63);
+            this.tbReceived.TabIndex = 10;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(72, 258);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(193, 93);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(477, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 44);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "SERVER";
+            // 
+            // backgroundWork
+            // 
+            this.backgroundWork.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWork_DoWork);
+            this.backgroundWork.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWork_ProgressChanged);
+            this.backgroundWork.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWork_RunWorkerCompleted);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(351, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(256, 44);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Search history";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1195, 583);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lvSearchHistory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbReceived);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Server";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SERVER _ NHÓM 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListView lvSearchHistory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbReceived;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWork;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Server/Server.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+        IPEndPoint ipendpoint;
+        Socket client;
+        TcpListener tcplistener;
+        string input;
+        string output;
+
+        public Server()
+        {
+            InitializeComponent();
+            backgroundWork.WorkerReportsProgress = true;
+            backgroundWork.WorkerSupportsCancellation = true;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnConnect_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //tạo 1 IP Endpoint mới
+                ipendpoint = new IPEndPoint(IPAddress.Any, 9999);
+                tcplistener = new TcpListener(ipendpoint);
+
+                //tạo thread mới để nhận dữ liệu 
+                //sử dụng arrow function để invok hàm chỉ khi ấn button, nếu không nó sẽ thành IIFE 
+                Thread thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        tcplistener.Start();
+                        client = tcplistener.AcceptSocket();
+                        Thread rec = new Thread(ReceiveData);
+                        rec.IsBackground = true;
+                        rec.Start(client);
+                    }
+                });
+
+                thread.IsBackground = true;
+                thread.Start();
+                //nếu kết nối thành công thì hiển thị ra cho user
+                MessageBox.Show("Successfully Connected !");
+
+            }
+            catch
+            {
+                MessageBox.Show("Can't connect, please try again later !");
+            }
+        }
+
+        void SendData(Socket client)
+        {
+            try
+            {
+                //mã hóa chuỗi thành các byte để gửi 
+                byte[] outputByte = Encoding.UTF8.GetBytes(output);
+                client.Send(outputByte);
+
+                //làm trống output, sẵn sàng nhận dữ liệu tiếp theo từ user 
+                output = null;
+            }
+            catch
+            {
+                //nếu không nhận được gì => đã đứt kết nối
+                MessageBox.Show("Word not found in database !");
+            }
+        }
+
+        void ReceiveData(Object obj)
+        {
+            try
+            {
+                while (true)
+                {
+                    //nhận dữ liệu từ người dùng ở dạng byte
+                    Socket client = obj as Socket;
+                    byte[] clientMsg = new byte[1024];
+                    client.Receive(clientMsg);
+
+                    //mã hóa nó thành chuỗi
+                    input = Encoding.UTF8.GetString(clientMsg);
+
+                    //hiển thị vào danh sách đã nhận và màn hình trạng thái 
+                    tbReceived.Text = input;
+                    lvSearchHistory.Items.Add(input);
+                    backgroundWork.RunWorkerAsync();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No signal, please try again");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void backgroundWork_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //thiết lập connection mới cho mySQL 
+            MySql.Data.MySqlClient.MySqlConnection dbConn = new MySql.Data.MySqlClient.MySqlConnection("Persist Security Info=False;server=localhost;database=dictionarydb;uid=root;password=123456");
+            MySqlCommand cmd = dbConn.CreateCommand();
+
+            //thiết lập câu lệnh truy vấn từ database 
+            cmd.CommandText = "SELECT * from tbl_edict WHERE word='" + tbReceived.Text + "'";
+            cmd.CommandType = CommandType.Text;
+
+            // thử mở database 
+            try
+            {
+                dbConn.Open();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Cann't open database. Error: " + err);
+                this.Close();
+            }
+
+            //đọc database và thực hiện truy vấn 
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //bỏ đi thành phần đầu và thứ 2, đọc từ vị trí thứ 3, vì 2 vị trí đầu là từ cần tìm, dữ liệu thừa 
+                output += reader[2].ToString() + "\n";
+                break;
+            }
+
+
+            //đóng database 
+            dbConn.Close();
+        }
+
+        private void backgroundWork_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            return;
+        }
+
+        private void backgroundWork_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //hiển thị sau khi tìm kiếm hoàn tất 
+            //gửi đi cho client 
+            rtbResult.Text = "STATUS: DONE \n" + output;
+            SendData(client);
+        }
+    }
+}
